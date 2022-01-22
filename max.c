7069210a3f2c6c70a3d445a7b894a718207f571c
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-01-22 16:59:16 +0100

    initial commit

---
+++ max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct TItem {
+    struct TItem * next;
+    char digit;
+} TITEM;
+
+TITEM * maxOf ( TITEM ** x, int nr ) {
+
+
+    return NULL;
+}
+
+int main ( void ) {
+
+    return 0;
+}
